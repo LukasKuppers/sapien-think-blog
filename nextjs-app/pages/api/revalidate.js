dd1fd7462bbdeaa3e50cda6ccbbae1a3fbe6be08
Lukas Kuppers <lkuppers11@gmail.com>
2023-09-22 12:09:04 -0700

    Add next cloud function to rebuild specific pages

---
+++ nextjs-app/pages/api/revalidate.js
@@ -0,0 +1,34 @@
+
+export default async function handler(req, res) {
+  console.log('[/api/revalidate] Processing request to rebuild page content.');
+  // check if the revalidate request is authenticated
+  if (req.query.secret !== process.env.REVALIDATE_REQUEST_TOKEN) {
+    console.log('[/api/revalidate] Aborting request, provided token is invalid.');
+    return res.status(401).json({
+      message: 'Provided token is invalid.'
+    });
+  }
+
+  // get article id
+  const articleId = req.query.articleId;
+  if (! articleId || articleId === '') {
+    console.error('[/api/revalidate] Aborting request does not provide article ID.');
+    return res.status(400).json({
+      error: 'article ID not provided in query parameters. Requests must include "articleId=<id>".'
+    });
+  }
+
+  try {
+    await res.revalidate(`/posts/${articleId}`);
+
+    console.log('[/api/revalidate] Success.')
+    return res.json({
+      revalidated: true
+    });
+  } catch (error) {
+    console.error('[/api/revalidate] Encountered error when rebuilding page:', error);
+    return res.status(500).json({
+      error: 'Internal server error when revalidating.', 
+    });
+  }
+}
