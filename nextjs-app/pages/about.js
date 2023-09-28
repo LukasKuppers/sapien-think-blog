3a9821ad2094655c2e84c26c3d807832030618ca
Lukas Kuppers <lkuppers11@gmail.com>
2023-09-27 20:17:45 -0700

    Create about page

---
+++ nextjs-app/pages/about.js
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+
+import Layout from "../components/layout";
+import styles from '../styles/about.module.css';
+import utilStyles from '../styles/utils.module.css';
+import { jetBrainsMonoBold, jetBrainsMono, merriweather } from '../lib/fonts';
+
+const text = require('../textChunks.json');
+
+
+const About  = () => {
+  return (
+    <Layout pageTitle={text.aboutTitle} pageDesc={text.aboutDesc}>
+      <div className={`${styles.container}`}>
+        <div className={styles.headingContainer}>
+          <Image src='/images/socrates.svg' width={200} height={200} />
+          <h1 className={`${utilStyles.colPrimary} ${jetBrainsMonoBold.className}`}>About</h1>
+        </div>
+        <div className={`${styles.contentContainer} ${jetBrainsMono.variable} 
+                         ${merriweather.variable} ${utilStyles.colPrimary}`}>
+          <p>{text.aboutIntro}</p>
+          <h2>Our Philosophy</h2>
+          <p>{text.aboutPhilosophy}</p>
+          <h2>Nurturing Curiosity</h2>
+          <p>{text.aboutNurturingCuriosity}</p>
+          <h2>Our Commitment</h2>
+          <p>{text.aboutCommitment}</p>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+
+export default About;
