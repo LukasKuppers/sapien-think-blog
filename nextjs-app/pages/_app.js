3b1a7b345986e8f2c91aaab90e4f8d958aaf32ac
Lukas Kuppers <lkuppers11@gmail.com>
2023-10-02 15:06:46 -0700

    Add google analytics to next app

--- nextjs-app/pages/_app.js
+++ nextjs-app/pages/_app.js
@@ -1,13 +1,25 @@
+import Script from 'next/script';
+
 import '../styles/globals.css';
 import utilStyles from '../styles/utils.module.css';
 
 
 const App = ({ Component, pageProps }) => {
-    return (
-        <div className={`app-wrapper ${utilStyles.colThemeBg}`}>
-            <Component {...pageProps} />
-        </div>
-    );
+  return (
+    <div className={`app-wrapper ${utilStyles.colThemeBg}`}>
+      {/* Google tag (gtag.js) */}
+      <Script strategy='lazyOnload' src="https://www.googletagmanager.com/gtag/js?id=G-1RFVFJFGX6"></Script>
+      <Script strategy='lazyOnload'>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-1RFVFJFGX6');`}
+      </Script>
+
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 
