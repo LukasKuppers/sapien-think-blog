14a568606a6d273e0713c49355fe08113834ab80
Lukas Kuppers <lkuppers11@gmail.com>
2023-10-04 09:56:57 -0700

    Make CMS api accept tags list

--- cms-api/functions/controllers/articlesController.js
+++ cms-api/functions/controllers/articlesController.js
@@ -61,6 +61,15 @@ const createArticle = (req, res) => {
     }
   }
 
+  // if tags included, ensure tags is an array
+  if (reqBody.hasOwnProperty('tags')) {
+    if (!Array.isArray(reqBody.tags)) {
+      return res.status(400).json({
+        error: 'Request tags are malformed: tags must be an array of strings.'
+      });
+    }
+  }
+
   // create/update document
   articlesDb.createArticle(reqBody)
     .then((success) => {
