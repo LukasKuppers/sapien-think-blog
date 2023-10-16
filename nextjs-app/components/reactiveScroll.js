e762228964c325ee0928ad61d0853836dcb7c685
Lukas Kuppers <lkuppers11@gmail.com>
2023-10-16 14:26:58 -0700

    style reactive scroll container

--- nextjs-app/components/reactiveScroll.js
+++ nextjs-app/components/reactiveScroll.js
@@ -1,10 +1,28 @@
+import React, { useRef, useEffect, useState } from 'react';
+
 import styles from './reactiveScroll.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 
 const ReactiveScroll = ({ children }) => {
+
+  const containerRef = useRef(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const overflow = container.scrollHeight > container.clientHeight;
+    setIsOverflowing(overflow);
+  }, []);
+
   return (
-    <div className={`${styles.container}`}>
-      {children}
+    <div ref={containerRef} className={`${isOverflowing ? styles.containerScroll : styles.container}`}>
+      {isOverflowing ? <div className={`${styles.blockerTop} ${utilStyles.colThemeBg}`} /> : ''}
+      <div className={styles.scrollContent}>
+        {children}
+      </div>
+      {isOverflowing ? <div className={`${styles.blockerBottom} ${utilStyles.colThemeBg}`} /> : ''}
     </div>
   );
 };
