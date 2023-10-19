9aecb27864efd194cfa44cf0f37ca49ff52fee2c
Lukas Kuppers <lkuppers11@gmail.com>
2023-10-19 11:55:41 -0700

    Remove opening and closing double quotes from reference search terms

--- nextjs-app/components/affiliateLinkCard.js
+++ nextjs-app/components/affiliateLinkCard.js
@@ -11,13 +11,16 @@ const ASSOCIATES_TRACKING_ID = 'sapienthink06-20';
    * @returns an array with two values, the first is the author(s), the second is the title of the work.
    */
 const extractAuthorsAndTitle = (reference) => {
+
+  console.log('DEBUG: ' + reference);
+
   // process author lists with middle name initials
   // collapses patterns like 'G. W. F.' to 'G.'
   const regex = /([A-Z])\.(\s[A-Z]\.)+/g;
   reference = reference.replace(regex, '$1.');
 
   // remove all " characters
-  reference = reference.replace(/"/g, '');
+  reference = reference.replace(/["“”]/g, '');
   
   const sections = reference.split('. ');
   const filteredSections = sections
